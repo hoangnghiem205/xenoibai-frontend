a6ce669a7f31e4be9b75ca33c973c8efa3c0b0c3
Nghiem Minh Hoang <hoangnm@ows.vn>
2016-12-09 00:09:56 +0700

    fix tab

--- app/home/home.js
+++ app/home/home.js
@@ -2,13 +2,33 @@
 
 angular.module('myApp.home', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {
-    templateUrl: 'home/home.html',
-    controller: 'HomeCtrl'
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/home', {
+      templateUrl: 'home/home.html',
+      controller: 'HomeCtrl'
+    });
+  }])
+
+  .controller('HomeCtrl', function ($scope) {
+    $scope.showTab1 = true;
+    $scope.showTab2 = false;
+    $scope.showTab3 = false;
+
+    $scope.showTab = function (index) {
+      if (index == 1) {
+        $scope.showTab1 = true;
+        $scope.showTab2 = false;
+        $scope.showTab3 = false;
+      }
+      else if (index == 2) {
+        $scope.showTab1 = false;
+        $scope.showTab2 = true;
+        $scope.showTab3 = false;
+      }
+      else {
+        $scope.showTab1 = false;
+        $scope.showTab2 = false;
+        $scope.showTab3 = true;
+      }
+    }
   });
-}])
-
-.controller('HomeCtrl', [function() {
-
-}]);
